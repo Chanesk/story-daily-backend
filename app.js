2b544c3168998e336c1ba0fef480a323e4f539e0
Chanesk <chanesk01@gmail.com>
2023-02-11 17:16:45 +0100

    feat: fonctionnalité signup

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config()
 const app = express();
+const userRoutes = require('./routes/user');
 
-mongoose.connect(`mongodb+srv://story:${process.env.PASSWORD}@cluster0.ocuowsl.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(`${process.env.CONNEXION_DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log('connexion mongoDb réussie'))
-.catch(() => console.log('connexion à mongoDb échouée !'));
+.catch((error) => console.log(error.message));
+
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app
